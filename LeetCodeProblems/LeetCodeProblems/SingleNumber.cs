b319668368e095f9f1294770662f24436ca12a52
Ola Alataiwi <olaabilal1@gmail.com>
2022-10-29 20:26:02 +0200

    Creating SingleNumber Class

---
+++ LeetCodeProblems/LeetCodeProblems/SingleNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems
+{
+    class SingleNumber
+    {
+
+        public static int singleNumber(int[] nums)
+        {
+            int r = 0;
+            foreach (var i in nums)
+                r ^= i;
+            return r;
+            //with no constrains mentioned in LeetCode Description
+            /*Dictionary<int, int> mydic = new Dictionary<int, int>();
+              foreach(var i in nums)
+              {
+                  if (mydic.ContainsKey(i) == false)
+                      mydic.Add(i, 1);
+                  else
+                      mydic[i] += 1;
+              }
+              foreach (KeyValuePair<int, int> pair in mydic)
+                  if (pair.Value == 1)
+                      return pair.Key;
+          return -1;*/
+        }
+    }
+}
