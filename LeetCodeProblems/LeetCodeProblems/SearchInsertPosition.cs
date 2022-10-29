7dcdbe4c61b90acd97d253762d7578dfb862163d
Ola Alataiwi <olaabilal1@gmail.com>
2022-10-29 20:28:00 +0200

    Creating SearchInsertPosition Class

---
+++ LeetCodeProblems/LeetCodeProblems/SearchInsertPosition.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems
+{
+    class SearchInsertPosition
+    {
+        public static int SearchInsert(int[] nums, int target)
+        {
+            //i will use binary search for time complixity constrain
+            int left = 0;
+            int right = nums.Length - 1;
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+                if (nums[mid] == target) return mid;
+                else if (nums[mid] > target) right = mid - 1;
+                else left = mid + 1;
+            }
+            return left;
+        }
+    }
+}
